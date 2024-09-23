f00a96f583ba2ed09a8d0f485bf3eb11d8c9f0af
Jaden <jaden.franks@gmail.com>
2024-09-23 17:47:20 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(name, email){
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+    })
+    
+  })
+  .then(resopnese => resopnese.json())
+  .then(data => { console.log(data)
+  document.body.append(data.id)})
+  .catch(function (error){
+    alert("problem")
+    document.body.append(error.message)
+  })
+} 
+
